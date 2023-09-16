e0a67556f209ed8bc474ace9caa12fc81cf08c0e
Christian Andrade <ce.andrade.p@gmail.com>
2023-09-15 21:18:12 -0500

    add header component and routes to new views on th navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import "./styles/global.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div>Hola</div>;
+  return (
+    <main>
+      <Header />
+    </main>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import About from "./views/About.jsx";
+import Careers from "./views/Careers.jsx";
+import Events from "./views/Events.jsx";
+import Products from "./views/Products.jsx";
+import Support from "./views/Support.jsx";
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
@@ -10,7 +16,23 @@ const router = createBrowserRouter([
   },
   {
     path: "/about",
-    element: <div>About</div>,
+    element: <About />,
+  },
+  {
+    path: "/careers",
+    element: <Careers />,
+  },
+  {
+    path: "/events",
+    element: <Events />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+  {
+    path: "/support",
+    element: <Support />,
   },
 ]);
 
